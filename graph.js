0364b816aa8aca63d9d3337f8a64f22d946209a0
Trewbot <trewbot@phene.co>
2018-07-13 09:46:06 -0500

    Set ticks to start at axes

--- graph.js
+++ graph.js
@@ -28,7 +28,7 @@ _g.g = (_g.graph = {
             PADDING_LEFT    = options.paddingLeft   || PADDING,
             PADDING_TOP     = options.paddingTop    || PADDING,
             ORIGIN          = options.origin        || {x:0.5,y:0.5};
-        function loadAxes(ctx, ticks=[100,4], labelMax=[100,1]){
+        function loadAxes(ctx, ticks=[100,40], labelMax=[100,40]){
             //	Calculate Positions
                 var width	= ctx.canvas.width  - (2*PADDING_LEFT),
                     height  = ctx.canvas.height - (2*PADDING_TOP),
@@ -39,13 +39,13 @@ _g.g = (_g.graph = {
             ctx.strokeStyle     = AXIS_COLOR;
             //  y-axis
             ctx.beginPath();
-            ctx.moveTo(left + (width*ORIGIN.x), top - 0.5);
-            ctx.lineTo(left + (width*ORIGIN.x), top + height);
+            ctx.moveTo(left + ~~(width*ORIGIN.x), top - 0.5);
+            ctx.lineTo(left + ~~(width*ORIGIN.x), top + height);
             ctx.stroke();
             //  x-axis
             ctx.beginPath();
-            ctx.moveTo(left - 0.5,   top + (height*ORIGIN.y));
-            ctx.lineTo(left + width, top + (height*ORIGIN.y));
+            ctx.moveTo(left - 0.5,   top + ~~(height*ORIGIN.y));
+            ctx.lineTo(left + width, top + ~~(height*ORIGIN.y));
             ctx.stroke();
             ctx.lineWidth       = TICK_WIDTH;
             ctx.strokeStyle     = TICK_COLOR;
@@ -53,38 +53,42 @@ _g.g = (_g.graph = {
             ctx.textAlign		= "center";
             ctx.textBaseline	= "top";
             ctx.fillStyle		= LABEL_COLOR;
-            //	Draw x-axis ticks
-            for(let i = 0; i <= ticks[0]; i++){
+            //	Draw x-axis Ticks
+            var xTick = i => {
                 //  Draw tick
                 ctx.beginPath();
-                let x = ~~((width / ticks[0]) * i) + left;
-                ctx.moveTo(x, 0.5 + top + (height*ORIGIN.y));
-                ctx.lineTo(x, 0.5 + top + (height*ORIGIN.y) + (
+                let x = ~~((width / ticks[0]) * i) + left + ~~(width*ORIGIN.x);
+                ctx.moveTo(x, 0.5 + top + ~~(height*ORIGIN.y));
+                ctx.lineTo(x, 0.5 + top + ~~(height*ORIGIN.y) + (
                     i % EXTEND_RATE == 0 ? EXTEND_LENGTH : TICK_LENGTH
                 ));
                 ctx.stroke();
-                //	Write labels
-                if(i % LABEL_RATE == 0 || ticks[0] < LABEL_RATE)
+                //	Write label
+                if((i % LABEL_RATE == 0 || ticks[0] < LABEL_RATE) && i != 0)
                     ctx.fillText((i / ticks[0]) * labelMax[0], x,
                         top + (height*ORIGIN.y) + EXTEND_LENGTH + 1);
-            }
+            };
+            for(let i = 0; i <= ticks[0]*ORIGIN.x; i++) xTick(i);
+            for(let i = 0; i >= -1*ticks[0]*(1-ORIGIN.x); i--) xTick(i);
+            //	Draw y-axis Ticks
             ctx.textAlign		= "right";
             ctx.textBaseline	= "middle";
-            //	Draw y-axis Ticks
-            for(let i = 0; i <= ticks[1]; i++){
+            var yTick = i => {
+                // Draw tick
                 ctx.beginPath();
-                let y = ~~((height / ticks[1]) * i) + top;
-                ctx.moveTo(0.5 + left + (width*ORIGIN.x), y);
-                ctx.lineTo(0.5 + left + (width*ORIGIN.x) - (
+                let y = ~~((height / ticks[1])*i)+top+~~(height*ORIGIN.y);
+                ctx.moveTo(0.5 + left + ~~(width*ORIGIN.x), y);
+                ctx.lineTo(0.5 + left + ~~(width*ORIGIN.x) - (
                     i % EXTEND_RATE == 0 ? EXTEND_LENGTH : TICK_LENGTH
                 ), y);
                 ctx.stroke();
-                //	Write Tick Labels
-                if(i % LABEL_RATE == 0 || ticks[1] < LABEL_RATE)
-                    ctx.fillText(
-                        ~~(((ticks[1]-i)/ticks[1])*labelMax[1]*1e4)/100+"%",
+                //	Write label
+                if((i % LABEL_RATE == 0 || ticks[1] < LABEL_RATE) && i != 0)
+                    ctx.fillText((-1 * i / ticks[1]) * labelMax[1],
                         left + (width*ORIGIN.x) - EXTEND_LENGTH - 1, y);
             }
+            for(let i = 0; i <= ticks[1]*ORIGIN.y; i++) yTick(i);
+            for(let i = 0; i >= -1*ticks[1]*(1-ORIGIN.y); i--) yTick(i);
             return {
                 "height" : height,
                 "left"   : left,
@@ -97,6 +101,7 @@ _g.g = (_g.graph = {
     },
     changes: [
         ["g0.1.0.0001","Jul 12, 2018","Initial"],
-        ["g0.1.0.0002","Jul 13, 2018","Shifted axes to origin point."]
+        ["g0.1.0.0002","Jul 13, 2018","Shifted axes to origin point."],
+        ["g0.1.0.0003","Jul 13, 2018","Set ticks to start at axes"]
     ]
 });
